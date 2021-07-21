34ea34b904280d192604b679be272116a9ae1290
Ldpavlov <simeonpavlov07@gmail.com>
2021-07-21 20:34:35 +0300

    initial commit

---
+++ MyWebApp-BikeShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyWebApp_BikeShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
