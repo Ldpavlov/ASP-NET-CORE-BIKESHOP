8184be2c70290507a8c74392cbcd0f4388da312a
Ldpavlov <simeonpavlov07@gmail.com>
2021-08-24 23:48:44 +0300

    Added Validations

--- MyWebApp-BikeShop/Models/Buyer/BecomeABuyerFormModel.cs
+++ MyWebApp-BikeShop/Models/Buyer/BecomeABuyerFormModel.cs
@@ -1,9 +1,20 @@
 ﻿namespace MyWebApp_BikeShop.Models.Buyer
 {
+    using System.ComponentModel.DataAnnotations;
+    using static MyWebApp_BikeShop.Data.DataConstants;
+
     public class BecomeABuyerFormModel
-    {        
+    {
+        [Required]
+        [StringLength(BuyerNameMaxLength, MinimumLength = BuyerNameMinLength)]
         public string FullName { get; set; }
+
+        [Required]
+        [StringLength(BuyerPhoneMaxLength, MinimumLength = BuyerPhoneMinLength)]
         public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(AddressMaxLength, MinimumLength = AddressMinLength)]
         public string Address { get; set; }
 
     }
